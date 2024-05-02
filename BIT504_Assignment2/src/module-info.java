d42d36da515f7c36fda081dedff7317df6630a7d
MelanieTownshend <melaniejtownshend@outlook.com>
2024-05-02 16:17:30 +1200

    upload of initial files

---
+++ BIT504_Assignment2/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BIT504_Assignment2 {
+}
---
+++ BIT504_Assignment2/src/tic_tac_toe/Player.java
@@ -0,0 +1,9 @@
+package tic_tac_toe;
+
+/**
+ * Enumeration for the players move
+ */
+
+public enum Player {
+	Empty, Cross, Nought
+}
